430f03513cdead319bf15209abab77cc1cd1b509
tomakor549 <venetor7@gmail.com>
2022-01-06 22:01:15 +0100

    first commit

---
+++ TaiMvc/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TaiMvc.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Localization { get; set;}
+    }
+}
---
+++ TaiMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaiMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
