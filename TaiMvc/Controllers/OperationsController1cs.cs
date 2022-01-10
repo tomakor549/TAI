49e7c6cc0611069175efacf21bb784c9932345dc
kinga-krawczyk <kinga-krawczyk71@wp.pl>
2022-01-10 18:45:17 +0100

    Upload

---
+++ TaiMvc/Controllers/OperationsController1cs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaiMvc.Controllers
+{
+    public class OperationsController1cs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
