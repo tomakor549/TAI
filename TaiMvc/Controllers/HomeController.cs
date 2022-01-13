7a53d13a513e180bc0e3cfdc42802f57cf477f85
kinga-krawczyk <kinga-krawczyk71@wp.pl>
2022-01-13 14:14:46 +0100

    all upload basic

--- TaiMvc/Controllers/HomeController.cs
+++ TaiMvc/Controllers/HomeController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TaiMvc.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using System.Data;
 using System.IO;
 using System.Collections.Generic;
 using System.Web;
 using Abp.AspNetCore.Mvc.Authorization;
+
 
 namespace TaiMvc.Controllers
 {
@@ -14,12 +17,15 @@ namespace TaiMvc.Controllers
     {
 
         private readonly ILogger<HomeController> _logger;
+        private Microsoft.AspNetCore.Hosting.IHostingEnvironment _env;
+        private string _dir;
 
-     
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
             _logger = logger;
+            _env = env;
+            _dir = _env.ContentRootPath;
         }
 
         public IActionResult Index()
@@ -30,20 +36,25 @@ namespace TaiMvc.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        public IActionResult Operations()
-        {
-
-            return View();
-        }
-        
+        }   
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        
+        //tu zaczyna się upload normalny
+        public IActionResult Operations() => View();
+        public IActionResult OperationUpload(IFormFile file)
+        {
+            using (var fileStream = new FileStream("path", FileMode.Create, FileAccess.Write))
+            {
+                if (file != null)
+                {
+                    file.CopyTo(fileStream);
+                }
+            }
+            return RedirectToAction("Operations");
+        }
     }
 }
